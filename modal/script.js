90f36645548e552dced635412c1a2aab4f8773c6
Clement Owireku-Bogyah <89473856+Zena4L@users.noreply.github.com>
2022-05-18 10:30:36 +0000

    Add files via upload

---
+++ modal/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+const btnShowModal = document.querySelectorAll(".show-modal");
+
+const showModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", showModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && e.key !== modal.classList.add("hidden")) {
+    closeModal();
+  }
+});
